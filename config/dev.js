004c8026a84a10093c8edd4a486aacb940a225ce
Derderon <36231878+Derderon@users.noreply.github.com>
2018-03-05 13:31:37 +0100

    second commit

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+//dev.js - don't commit this
+module.exports = {
+	googleClientID:
+		"82106601775-emiitoso65v33ar6aqdrq7e2he531vad.apps.googleusercontent.com",
+	googleClientSecret: "Tzb2tmIagI0R-FUz0ZbxloG0",
+	mongoURI: "mongodb://mexter:mexter@ds127888.mlab.com:27888/feedback-dev",
+	cookieKey: "ifdisfisdsfkdsfsgbnfdswsdadm",
+	stripePublishableKey: "pk_test_e6fXdTLEScI51WmjzM5frwM8",
+	stripeSecretKey: "sk_test_U1JYOVFVwJCaYnbex7XlkWQI",
+	sendGridKey: "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",
+	redirectDomain: "http://localhost:3000"
+};
